dc025547ee0e0a9081608cf2cbdc5139885754e1
dlthdud0901 <dlthdud0901@gmail.com>
2026-07-09 15:51:25 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.SCHEDULE_APP_CONFIG = {
   SUPABASE_URL: "https://jhukybjwxmpynjdykyyv.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_DuFzQGtWKWNgx4GOa3dmvA_JU6_iGjR",
   STATE_ID: "main",
-  VAPID_PUBLIC_KEY: ""
+  VAPID_PUBLIC_KEY: "BLBPwC6p5EhBY61cny4fhMVMm4raVFF0VpvEdLcnZrVbzZmSb4VzphWcck-URR_1HV2TbeN4RZSnSIFO4UVJUt0"
 };
